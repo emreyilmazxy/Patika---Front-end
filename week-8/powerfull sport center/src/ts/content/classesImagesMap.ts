3ff92aef6f721718edd184e9590cf4da8979bb43
Emre Yılmaz <emreyilmazxy@gmail.com>
2025-12-01 04:00:25 +0300

    deploy photos

---
+++ week-8/powerfull sport center/src/ts/content/classesImagesMap.ts
@@ -0,0 +1,14 @@
+
+//for vite deploy issues
+
+import yogaImage from "/src/assets/images/yoga.jpg";
+import groupImage from "/src/assets/images/group.webp";
+import stretchImage from "/src/assets/images/stretch.jpg";
+import soloImage from "/src/assets/images/solo.jpg";
+
+export const CLASSES_IMAGES_MAP: Record<string, string> = {
+    "yoga-btn": yogaImage,
+    "group-btn": groupImage,
+    "stretch-btn": stretchImage,
+    "solo-btn": soloImage,
+};
