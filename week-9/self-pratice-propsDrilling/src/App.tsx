63de92c5420c9ec258407374e55d75269b9d4d5e
Emre Yılmaz <emreyilmazxy@gmail.com>
2025-12-13 20:27:00 +0300

    first commit

---
+++ week-9/self-pratice-propsDrilling/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Parent from './components/Parent';
+
+function App() {
+  const message = 'merhaba emre ';
+
+  return (
+    <>
+      <Parent message={message} />
+    </>
+  )
+}
+
+export default App
---
+++ week-9/self-pratice-propsDrilling/src/components/parent.jsx
@@ -0,0 +1,11 @@
+function Parent({ message }) {
+
+    return (
+        <div>
+            <h1>Parent Component</h1>
+
+        </div>
+    )
+}
+
+export default Parent;
